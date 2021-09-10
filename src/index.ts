4885154118e5c8441c94fdd3f618362a4cd961e5
camilyAbade <laceyj2.laceyj2@gmail.com>
2021-09-10 16:20:35 -0300

    add gitignore

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,7 @@ app.use("/buy", purchaseRouter)
 app.use("/stock", stockRouter)
 
 
-const server = app.listen(3000, () => {
-    if (server) {
-      const address = server.address() as AddressInfo;
-      console.log(`Servidor rodando em http://localhost:${address.port}`);
-    } else {
-      console.error(`Falha ao rodar o servidor.`);
-    }
-  });
+  const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`App is running on port ${ PORT }`);
+});
