cbdeb4554f0927d3a40577afdcb33bd3a40f1f1b
camilyAbade <laceyj2.laceyj2@gmail.com>
2021-09-11 11:19:12 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,13 @@ import express from "express";
 import { productRouter } from "./routes/productRouter";
 import { purchaseRouter } from "./routes/purchaseRouter";
 import { stockRouter } from "./routes/stockRouter";
+import cors from 'cors'
+
 dotenv.config();
+
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 
@@ -13,8 +18,8 @@ app.use("/list", productRouter)
 app.use("/buy", purchaseRouter)
 app.use("/stock", stockRouter)
 
-
-  const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3010;
 app.listen(PORT, () => {
     console.log(`App is running on port ${ PORT }`);
 });
+
